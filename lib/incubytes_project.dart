3de9b03598589c2a8b7f4a3daa8074bf51e90e89
ravikumar pabboju <swathippabboju@gmail.com>
2025-08-29 09:37:56 +0530

    numbers separated by new line

--- lib/incubytes_project.dart
+++ lib/incubytes_project.dart
@@ -1,12 +1,22 @@
 int add(String input) {
-  if (input.isEmpty) return 0; // Implemented logic for empty string
+  if (input.isEmpty) return 0; // Handle empty string
 
-  if ((input.length == 1) && (RegExp(r'^\d+$').hasMatch(input))) {
-    int.tryParse(input) ?? 0; // Implemented logic for single number
+  // Ensure input contains at least one digit
+  if (!RegExp(r'\d').hasMatch(input)) return 0;
+
+
+ 
+
+  // Single number case
+  if ((input.length == 1) && RegExp(r'^\d+$').hasMatch(input)) {
+    int number = int.tryParse(input) ?? 0;
+    if (number < 0) throw Exception("negative numbers not allowed $number");
+    return number;
   }
 
-  //any amount of numbers splitted  by comma
-  final parts = input.split(',');
-  return parts.map(int.parse).reduce((a, b) => a + b);
+    // Default delimiter regex
+  final delimiterRegex = RegExp(r'[,;\-\s|\n]+');
+  print(delimiterRegex.hasMatch(input));
 
+ return 0;
 }
--- test/incubytes_project_test.dart
+++ test/incubytes_project_test.dart
@@ -13,5 +13,15 @@ void main() {
   test("any amount of numbers seperated by commas", () {
     expect(add("6,1,3"), 10);
   });
-  
+
+
+  test('sums numbers separated by commas and newlines', () {
+    expect(add('1\n2,3'), equals(6));
+  });
+  test('numbers separated by newline', () {
+    expect(add('1\n2\n3'), equals(6));
+  });
+
+ 
+   
 }
